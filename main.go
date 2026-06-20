ae2b36784dbed4d075ece83bceba9b262b81cc15
Pavel Denisov <padeniso@gmail.com>
2026-06-20 14:42:30 +0300

    1-st commit

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"time"
+)
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func newBin(id string, private bool, createdAt time.Time, name string) *Bin {
+
+}
+
 func main() {
+	binList := []Bin{}
 
 }
